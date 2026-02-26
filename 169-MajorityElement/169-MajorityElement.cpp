f333c71a766436e2a6af065e7f9b5605e6dc952e
samshyam25 <samshyamgod26@gmail.com>
2026-02-26 09:54:36 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,9 @@
+// Last updated: 2/26/2026, 9:54:36 AM
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n/2];
+    }
+};
