628b124cf0e1e675ac305c21bed9bbff1f34b200
samshyam25 <samshyamgod26@gmail.com>
2026-02-26 09:54:16 +0530

    Create 199-BinaryTreeRightSideView

---
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.java
@@ -0,0 +1,25 @@
+// Last updated: 2/26/2026, 9:54:15 AM
+import java.util.*;
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                // last node at this level
+                if (i == size - 1) res.add(node.val);
+                // add kids
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+        }
+        return res;
+    }
+}
