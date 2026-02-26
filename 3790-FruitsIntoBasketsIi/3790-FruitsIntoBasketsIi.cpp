e79dd919b05b16a52e4bd4adf693c7db9e965dad
samshyam25 <samshyamgod26@gmail.com>
2026-02-26 09:53:08 +0530

    Create 3790-FruitsIntoBasketsIi

---
+++ 3790-FruitsIntoBasketsIi/3790-FruitsIntoBasketsIi.cpp
@@ -0,0 +1,20 @@
+// Last updated: 2/26/2026, 9:53:07 AM
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        int alloted = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (fruits[i] <= baskets[j]) {
+                    alloted++;
+                    baskets[j] = -1; 
+                    break;
+                }
+            }
+        }
+
+        return n - alloted;
+    }
+};
