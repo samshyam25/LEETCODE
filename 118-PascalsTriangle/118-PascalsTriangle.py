05edbdcf4b18e1473a8815388c9d09357c46ea0b
samshyam25 <samshyamgod26@gmail.com>
2026-02-26 09:54:57 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.py
@@ -0,0 +1,16 @@
+# Last updated: 2/26/2026, 9:54:56 AM
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+        
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
+        
+        for i in range(1, numRows - 1):
+            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+        
+        prevRows.append(newRow)
+        return prevRows
